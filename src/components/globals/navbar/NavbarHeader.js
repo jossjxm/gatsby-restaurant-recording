a67fb137e6b0bd054ec8a62fc0bb98b3fb897e2b
Jose Varillas <josevarillas24@gmail.com>
2019-05-01 14:52:59 -0500

    navbar added

---
+++ src/components/globals/navbar/NavbarHeader.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Link } from 'gatsby';
+import logo from '../../../images/logo.svg';
+import { FaAlignRight } from 'react-icons/fa';
+import styled from 'styled-components';
+import { styles } from '../../../utils';
+
+export default class NavbarHeader extends Component {
+  render() {
+    const { handleNavbar } = this.props;
+    return (
+      <HeaderWrapper>
+        <Link to="/">
+          <img src={logo} alt="company name" />
+        </Link>
+        <FaAlignRight
+          className="toogle-icon"
+          onClick={() => {handleNavbar()}}
+        />
+      </HeaderWrapper>
+    )
+  }
+}
+
+const HeaderWrapper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .toogle-icon {
+    font-size: 1.75rem;
+    color: ${styles.colors.mainYellow};
+    cursor: pointer;
+  }
+  @media (min-width: 768px) {
+    .toogle-icon {
+      display: none;
+    }
+  }
+`
